9135272a3f8ccd3e1a1c5e975e0d8b509c97697e
Andrei Kurosh <mail@spark.ms>
2025-05-08 15:18:44 +0200

    Repro code.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace FocusBugRepro.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        [ObservableProperty] private bool _isOpen;
+
+        [RelayCommand]
+        public void Open() => IsOpen = true;
+
+        [RelayCommand]
+        public void Close() => IsOpen = false;
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FocusBugRepro.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
